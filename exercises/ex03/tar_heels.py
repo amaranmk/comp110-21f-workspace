692bdae9fc099119642ee5d5e80aa06e362b1295
Amaran Kandiar <akandiar@unc.edu>
2021-09-13 23:45:59 -0400

    Completed tar heels exercises

--- exercises/ex03/tar_heels.py
+++ exercises/ex03/tar_heels.py
@@ -1,6 +1,22 @@
 """An exercise in remainders and boolean logic."""
 
-__author__ = "ENTER YOUR 9-DIGIT PID HERE"
+__author__ = "730484862"
 
 
 # Begin your solution here...
+user_integer: int = int(input("Enter an int:"))
+divisble_by_two: int = user_integer % 2
+divisble_by_seven: int = user_integer % 7
+
+if divisble_by_two == 0:
+    if divisble_by_seven == 0:
+        print("TAR HEELS")
+    else:
+        print("TAR")
+else:
+    if divisble_by_seven == 0:
+        print("HEELS")
+
+if divisble_by_seven != 0:
+    if divisble_by_two != 0:
+        print("CAROLINA")
