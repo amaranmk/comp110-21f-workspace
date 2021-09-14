8d295e8f2b86aac774b1c467507ddd8d4bef1f3b
Amaran Kandiar <akandiar@unc.edu>
2021-09-14 01:31:20 -0400

    Final touches

--- exercises/ex03/happy_trees.py
+++ exercises/ex03/happy_trees.py
@@ -1,6 +1,17 @@
 """Drawing forests in a loop."""
 
-__author__ = "123456789"
+__author__ = "730484862"
 
 # The string constant for the pine tree emoji
 TREE: str = '\U0001F332'
+
+output: str = ""
+i: int = 0
+j: int = 0
+
+user_depth: int = int(input("Depth: "))
+
+while i < user_depth:
+    output = output + TREE
+    print(output)
+    i = i + 1
