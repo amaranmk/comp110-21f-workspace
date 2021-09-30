2dcfc200ed24552f753c70588b73bd411f626e1d
Amaran Kandiar <akandiar@unc.edu>
2021-09-29 22:22:16 -0400

    add space

--- lessons/for_in.py
+++ lessons/for_in.py
@@ -2,7 +2,7 @@
 
 names: list[str] = ["Kris", "Kaki", "Ezri", "Marc"]
 
-#Example of iterating through names using a while loop
+# Example of iterating through names using a while loop
 print("while output:")
 i: int = 0
 while i < len(names):
@@ -10,7 +10,7 @@ while i < len(names):
     print(name)
     i += 1
 
-#The following for..in loop is the same as the while loop
+# The following for..in loop is the same as the while loop
 print("for..in output:")
 for name in names:
     print(name)
